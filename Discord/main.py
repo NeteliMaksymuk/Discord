fac302aab5997aaa5032312c376543a7459f8bac
¨NeteliMaksymuk¨ <natavaksumyk@gmail.com>
2022-08-27 10:58:18 +0300

    change input data

--- Discord/main.py
+++ Discord/main.py
@@ -1,52 +1,54 @@
 import requests
 import json
+import random
 
 headers = {
     'user-agent': 'Mozilla/5.0'
 }
 
 payload = {
-      "fingerprint": "",
-      "email": "",
-      "username": "",
-      "password": "",
-      "invite": None,
-      "consent": True,
-      "date_of_birth": "2000-01-01",
-      "gift_code_sku_id": None,
-      "captcha_key": None
+    "fingerprint": "fingerprint",
+    "email": "email",
+    "username": "username",
+    "password": "password",
+    "invite": None,
+    "consent": True,
+    "date_of_birth": "2000-01-01",
+    "gift_code_sku_id": None,
+    "captcha_key": None
 }
-payload=json.dumps(payload)
+
+email = input('Enter email: ')
+username = input('Username: ')
+password = ''
+
+for x in range(9):
+    password = password + random.choice(list(
+        '1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'
+    ))
+
+print('Your password is: ', password)
+
+payload['email'] = email
+payload['username'] = username
+payload['password'] = password
+payload = json.dumps(payload)
 
 session = requests.Session()
 
-k=session.post('https://discord.com/api/v8/auth/fingerprint', headers=headers,data=payload)
+responce_fingerprint = session.post('https://discord.com/api/v8/auth/fingerprint', headers=headers, data=payload)
+payload = json.loads(payload)
 
-payload = {
-          "fingerprint": "",
-          "email": "djasllsllgggggflfest@gmail.com",
-          "username": "netelalalaltest",
-          "password": "lglglitestproject",
-          "invite": None,
-          "consent": True,
-          "date_of_birth": "2000-01-01",
-          "gift_code_sku_id": None,
-          "captcha_key": None
-}
-payload['fingerprint']=str(k.json()['fingerprint'])
+payload['fingerprint'] = str(responce_fingerprint.json()['fingerprint'])
 
-
-payload=json.dumps(payload)
-
+payload = json.dumps(payload)
 session = requests.Session()
-p = session.post('https://discord.com/api/v9/auth/register', headers=headers,data=payload)
-print(p.status_code)
-print(p.text)
-print(p.cookies)
+responce_register = session.post('https://discord.com/api/v9/auth/register', headers=headers, data=payload)
 
-
-
-
-
-
-
+if responce_register.status_code == 201:
+    print(responce_register.status_code, '\n', responce_register.text)
+elif responce_register.status_code == 429:
+    print('Take it easy')
+else:
+    print('Check your email or username')
+    print(responce_register.text)
